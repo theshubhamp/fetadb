be77a38cc0ac74c31d543f539d903c335d30ad68
Shubham Patil <theshubhamp@gmail.com>
2024-06-07 16:22:53 +0530

    Add simple PG wire compatible server

--- fetadb.go
+++ fetadb.go
@@ -1,29 +1,86 @@
 package main
 
 import (
-	"fmt"
-	sql "github.com/pganalyze/pg_query_go/v5"
-	"github.com/syndtr/goleveldb/leveldb"
+	"flag"
+	pgx "github.com/jackc/pgx/v5/pgproto3"
+	_ "github.com/pganalyze/pg_query_go/v5"
+	_ "github.com/syndtr/goleveldb/leveldb"
 	"log"
+	"net"
 )
 
+var options struct {
+	listenAddress string
+	remoteAddress string
+}
+
 func main() {
-	db, err := leveldb.OpenFile("path/to/db", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	flag.StringVar(&options.listenAddress, "listen", "127.0.0.1:5432", "Listen address")
 
-	err = db.Put([]byte("key"), []byte("value"), nil)
+	listener, err := net.Listen("tcp", options.listenAddress)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to setup listener: %v", err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("failed to accept connection: %v", err)
+		}
+		go handleIncomingConnection(conn)
+	}
+}
+
+func handleIncomingConnection(conn net.Conn) {
+	backend := pgx.NewBackend(conn, conn)
+
+	msg, err := backend.ReceiveStartupMessage()
+	if err != nil {
+		log.Printf("failed to accept connection: %v", err)
+		return
+	}
+	log.Printf("connection established from remote: %v", conn.RemoteAddr())
+
+	if _, ok := msg.(*pgx.SSLRequest); ok {
+		// deny ssl request
+		_, err = conn.Write([]byte{'N'})
+		if err != nil {
+			log.Printf("failed to respond to ssl request: %v", err)
+			return
+		}
+
+		// re-receive startup message after responding to ssl request
+		_, err := backend.ReceiveStartupMessage()
+		if err != nil {
+			log.Printf("failed to accept connection: %v", err)
+			return
+		}
+	} else if _, ok := msg.(*pgx.StartupMessage); ok {
+		// got startup message, ok to proceed.
+	} else {
+		log.Printf("unsupported startup hanshake: %T", msg)
+		return
 	}
 
-	result, err := sql.Parse("SELECT 42")
+	backend.Send(&pgx.AuthenticationOk{})
+	backend.Send(&pgx.ParameterStatus{Name: "server_version", Value: "16.0"})
+	backend.Send(&pgx.BackendKeyData{ProcessID: 0, SecretKey: 0})
+	backend.Send(&pgx.ReadyForQuery{TxStatus: 'I'})
+
+	err = backend.Flush()
 	if err != nil {
-		panic(err)
+		log.Printf("failed to flush: %v", err)
+		return
 	}
 
-	// This will output "42"
-	fmt.Printf("%d\n", result.Stmts[0].Stmt.GetSelectStmt().GetTargetList()[0].GetResTarget().GetVal().GetAConst().GetIval().Ival)
+	for {
+		msg, err := backend.Receive()
+		if err != nil {
+			log.Printf("failed to receive message: %v", err)
+			return
+		}
+
+		log.Printf("received message from connection: %T(%v)", msg, msg)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module fetadb
 go 1.22
 
 require (
+	github.com/jackc/pgx/v5 v5.6.0
 	github.com/pganalyze/pg_query_go/v5 v5.1.0
 	github.com/syndtr/goleveldb v1.0.0
 )
@@ -10,8 +11,10 @@ require (
 require (
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
