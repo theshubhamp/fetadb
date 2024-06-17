d8b41b2d7eac478538f40c0a5f70b2bc5b60e6fd
Shubham Patil <theshubhamp@gmail.com>
2024-06-17 17:38:05 +0530

    Support evaluating expressions

--- pkg/sql/expr.go
+++ pkg/sql/expr.go
@@ -2,16 +2,26 @@ package sql
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
+type EvaluationContext interface {
+	LookupColumnRef(ref ColumnRef) any
+}
+
 type Expression interface {
+	Evaluate(ec EvaluationContext) any
 	String() string
 }
 
 type Equals struct {
 	Left  Expression
 	Right Expression
+}
+
+func (e Equals) Evaluate(ec EvaluationContext) any {
+	return reflect.DeepEqual(e.Left.Evaluate(ec), e.Right.Evaluate(ec))
 }
 
 func (e Equals) String() string {
@@ -22,12 +32,20 @@ type ColumnRef struct {
 	Names []string
 }
 
+func (c ColumnRef) Evaluate(ec EvaluationContext) any {
+	return ec.LookupColumnRef(c)
+}
+
 func (c ColumnRef) String() string {
 	return strings.Join(c.Names, ".")
 }
 
 type Literal struct {
 	Value any
+}
+
+func (l Literal) Evaluate(ec EvaluationContext) any {
+	return l.Value
 }
 
 func (l Literal) String() string {
