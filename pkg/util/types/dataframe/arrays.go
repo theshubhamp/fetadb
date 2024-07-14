4676d85c57cc446604b307f746839db5765aa6ed
Shubham Patil <theshubhamp@gmail.com>
2024-07-15 00:54:58 +0530

    Simplify arrays.go with generics

--- pkg/util/types/dataframe/arrays.go
+++ pkg/util/types/dataframe/arrays.go
@@ -9,7 +9,7 @@ type Array interface {
 	New() Array
 	Length() int
 	Get(i int) any
-	Append(val any) Array
+	Append(val any)
 	Equals(i int, j int) bool
 	Less(i int, j int) bool
 	Swap(i int, j int)
@@ -18,81 +18,90 @@ type Array interface {
 func NewArray(val any) Array {
 	switch val.(type) {
 	case string:
-		array := NewArrayString()
-		return array.Append(val)
+		array := NewArrayOrdered[string]()
+		array.Append(val)
+		return &array
 	case bool:
 		array := NewArrayBool()
-		return array.Append(val)
+		array.Append(val)
+		return &array
 	case int64:
-		array := NewArrayInt64()
-		return array.Append(val)
+		array := NewArrayOrdered[int64]()
+		array.Append(val)
+		return &array
 	case uint64:
-		array := NewArrayUint64()
-		return array.Append(val)
+		array := NewArrayOrdered[uint64]()
+		array.Append(val)
+		return &array
 	case float64:
-		array := NewArrayFloat64()
-		return array.Append(val)
+		array := NewArrayOrdered[float64]()
+		array.Append(val)
+		return &array
 	default:
 		array := NewArrayAny()
-		return array.Append(val)
+		array.Append(val)
+		return &array
 	}
 }
 
-type items[T any] struct {
-	items []T
-	nulls map[int]bool
+type items[T comparable] struct {
+	values []T
+	nulls  map[int]bool
 }
 
-type ArrayString items[string]
-
-func NewArrayString() ArrayString {
-	return ArrayString{items: []string{}, nulls: map[int]bool{}}
+func (it *items[T]) Length() int {
+	return len(it.values)
 }
 
-func (a *ArrayString) New() Array {
-	created := NewArrayString()
-	return &created
-}
-
-func (a *ArrayString) Length() int {
-	return len(a.items)
-}
-
-func (a *ArrayString) Get(i int) any {
-	if has, ok := a.nulls[i]; ok && has {
+func (it *items[T]) Get(i int) any {
+	if has, ok := it.nulls[i]; ok && has {
 		return nil
 	}
 
-	return a.items[i]
+	return it.values[i]
 }
 
-func (a *ArrayString) Append(val any) Array {
+func (it *items[T]) Append(val any) {
 	if val == nil {
-		a.nulls[len(a.items)] = true
-		val = ""
+		it.nulls[len(it.values)] = true
+		val = *new(T)
 	}
 
-	a.items = append(a.items, val.(string))
-	return a
+	it.values = append(it.values, val.(T))
 }
 
-func (a *ArrayString) Equals(i int, j int) bool {
-	return a.items[i] == a.items[j]
+func (it *items[T]) Equals(i int, j int) bool {
+	return it.values[i] == it.values[j]
 }
 
-func (a *ArrayString) Less(i int, j int) bool {
-	return a.items[i] < a.items[j]
+func (it *items[T]) Swap(i int, j int) {
+	it.values[i], it.values[j] = it.values[j], it.values[i]
+	it.nulls[i], it.nulls[j] = it.nulls[j], it.nulls[i]
 }
 
-func (a *ArrayString) Swap(i int, j int) {
-	a.items[i], a.items[j] = a.items[j], a.items[i]
-	a.nulls[i], a.nulls[j] = a.nulls[j], a.nulls[i]
+type ArrayOrdered[T cmp.Ordered] struct {
+	*items[T]
 }
 
-type ArrayBool items[bool]
+func NewArrayOrdered[T cmp.Ordered]() ArrayOrdered[T] {
+	return ArrayOrdered[T]{items: &items[T]{values: []T{}, nulls: map[int]bool{}}}
+}
+
+func (a *ArrayOrdered[T]) New() Array {
+	created := NewArrayOrdered[T]()
+	return &created
+}
+
+func (a *ArrayOrdered[T]) Less(i int, j int) bool {
+	return a.values[i] < a.values[j]
+}
+
+type ArrayBool struct {
+	*items[bool]
+}
 
 func NewArrayBool() ArrayBool {
-	return ArrayBool{items: []bool{}, nulls: map[int]bool{}}
+	return ArrayBool{items: &items[bool]{values: []bool{}, nulls: map[int]bool{}}}
 }
 
 func (a *ArrayBool) New() Array {
@@ -100,183 +109,16 @@ func (a *ArrayBool) New() Array {
 	return &created
 }
 
-func (a *ArrayBool) Length() int {
-	return len(a.items)
-}
-
-func (a *ArrayBool) Get(i int) any {
-	if has, ok := a.nulls[i]; ok && has {
-		return nil
-	}
-
-	return a.items[i]
-}
-
-func (a *ArrayBool) Append(val any) Array {
-	if val == nil {
-		a.nulls[len(a.items)] = true
-		val = false
-	}
-
-	a.items = append(a.items, val.(bool))
-	return a
-}
-
-func (a *ArrayBool) Equals(i int, j int) bool {
-	return a.items[i] == a.items[j]
-}
-
 func (a *ArrayBool) Less(i int, j int) bool {
-	return !a.items[i]
+	return !a.values[i]
 }
 
-func (a *ArrayBool) Swap(i int, j int) {
-	a.items[i], a.items[j] = a.items[j], a.items[i]
-	a.nulls[i], a.nulls[j] = a.nulls[j], a.nulls[i]
+type ArrayAny struct {
+	*items[any]
 }
-
-type ArrayInt64 items[int64]
-
-func NewArrayInt64() ArrayInt64 {
-	return ArrayInt64{items: []int64{}, nulls: map[int]bool{}}
-}
-
-func (a *ArrayInt64) New() Array {
-	created := NewArrayInt64()
-	return &created
-}
-
-func (a *ArrayInt64) Length() int {
-	return len(a.items)
-}
-
-func (a *ArrayInt64) Get(i int) any {
-	if has, ok := a.nulls[i]; ok && has {
-		return nil
-	}
-
-	return a.items[i]
-}
-
-func (a *ArrayInt64) Append(val any) Array {
-	if val == nil {
-		a.nulls[len(a.items)] = true
-		val = int64(0)
-	}
-
-	a.items = append(a.items, val.(int64))
-	return a
-}
-
-func (a *ArrayInt64) Equals(i int, j int) bool {
-	return a.items[i] == a.items[j]
-}
-
-func (a *ArrayInt64) Less(i int, j int) bool {
-	return a.items[i] < a.items[j]
-}
-
-func (a *ArrayInt64) Swap(i int, j int) {
-	a.items[i], a.items[j] = a.items[j], a.items[i]
-	a.nulls[i], a.nulls[j] = a.nulls[j], a.nulls[i]
-}
-
-type ArrayUint64 items[uint64]
-
-func NewArrayUint64() ArrayUint64 {
-	return ArrayUint64{items: []uint64{}, nulls: map[int]bool{}}
-}
-
-func (a *ArrayUint64) New() Array {
-	created := NewArrayUint64()
-	return &created
-}
-
-func (a *ArrayUint64) Length() int {
-	return len(a.items)
-}
-
-func (a *ArrayUint64) Get(i int) any {
-	if has, ok := a.nulls[i]; ok && has {
-		return nil
-	}
-
-	return a.items[i]
-}
-
-func (a *ArrayUint64) Append(val any) Array {
-	if val == nil {
-		a.nulls[len(a.items)] = true
-		val = uint64(0)
-	}
-
-	a.items = append(a.items, val.(uint64))
-	return a
-}
-
-func (a *ArrayUint64) Equals(i int, j int) bool {
-	return a.items[i] == a.items[j]
-}
-
-func (a *ArrayUint64) Less(i int, j int) bool {
-	return a.items[i] < a.items[j]
-}
-
-func (a *ArrayUint64) Swap(i int, j int) {
-	a.items[i], a.items[j] = a.items[j], a.items[i]
-	a.nulls[i], a.nulls[j] = a.nulls[j], a.nulls[i]
-}
-
-type ArrayFloat64 items[float64]
-
-func NewArrayFloat64() ArrayFloat64 {
-	return ArrayFloat64{items: []float64{}, nulls: map[int]bool{}}
-}
-
-func (a *ArrayFloat64) New() Array {
-	created := NewArrayFloat64()
-	return &created
-}
-
-func (a *ArrayFloat64) Length() int {
-	return len(a.items)
-}
-
-func (a *ArrayFloat64) Get(i int) any {
-	if has, ok := a.nulls[i]; ok && has {
-		return nil
-	}
-
-	return a.items[i]
-}
-
-func (a *ArrayFloat64) Append(val any) Array {
-	if val == nil {
-		a.nulls[len(a.items)] = true
-		val = float64(0)
-	}
-
-	a.items = append(a.items, val.(float64))
-	return a
-}
-
-func (a *ArrayFloat64) Equals(i int, j int) bool {
-	return a.items[i] == a.items[j]
-}
-
-func (a *ArrayFloat64) Less(i int, j int) bool {
-	return a.items[i] < a.items[j]
-}
-
-func (a *ArrayFloat64) Swap(i int, j int) {
-	a.items[i], a.items[j] = a.items[j], a.items[i]
-	a.nulls[i], a.nulls[j] = a.nulls[j], a.nulls[i]
-}
-
-type ArrayAny items[any]
 
 func NewArrayAny() ArrayAny {
-	return ArrayAny{items: []any{}, nulls: map[int]bool{}}
+	return ArrayAny{items: &items[any]{values: []any{}, nulls: map[int]bool{}}}
 }
 
 func (a *ArrayAny) New() Array {
@@ -284,37 +126,6 @@ func (a *ArrayAny) New() Array {
 	return &created
 }
 
-func (a *ArrayAny) Length() int {
-	return len(a.items)
-}
-
-func (a *ArrayAny) Get(i int) any {
-	if has, ok := a.nulls[i]; ok && has {
-		return nil
-	}
-
-	return a.items[i]
-}
-
-func (a *ArrayAny) Append(val any) Array {
-	if val == nil {
-		a.nulls[len(a.items)] = true
-		val = nil
-	}
-
-	a.items = append(a.items, val)
-	return a
-}
-
-func (a *ArrayAny) Equals(i int, j int) bool {
-	return a.items[i] == a.items[j]
-}
-
 func (a *ArrayAny) Less(i int, j int) bool {
-	return cmp.Less(fmt.Sprintf("%v", a.items[i]), fmt.Sprintf("%v", a.items[j]))
-}
-
-func (a *ArrayAny) Swap(i int, j int) {
-	a.items[i], a.items[j] = a.items[j], a.items[i]
-	a.nulls[i], a.nulls[j] = a.nulls[j], a.nulls[i]
+	return cmp.Less(fmt.Sprintf("%v", a.values[i]), fmt.Sprintf("%v", a.values[j]))
 }
