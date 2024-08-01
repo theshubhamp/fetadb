170fb42fe0fbd07628936277623b2546ef2596bf
Shubham Patil <theshubhamp@gmail.com>
2024-08-01 14:01:34 +0530

    Add test for memcmp order for integer encoding

--- pkg/kv/encoding/encoding_test.go
+++ pkg/kv/encoding/encoding_test.go
@@ -1,6 +1,7 @@
 package encoding
 
 import (
+	"bytes"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -55,4 +56,24 @@ func TestNegativeIntegerRoundTrip(t *testing.T) {
 	decoded = Decode(encoded)
 	require.NotNil(t, decoded)
 	require.Equal(t, int64(-13), decoded)
+}
+
+func TestIntegerMemcmp(t *testing.T) {
+	negative, err := Encode(int8(-13))
+	require.Nil(t, err)
+
+	positive, err := Encode(int8(13))
+	require.Nil(t, err)
+
+	cmpResult := bytes.Compare(negative, positive)
+	require.Equal(t, -1, cmpResult)
+
+	negative, err = Encode(int8(-13))
+	require.Nil(t, err)
+
+	positive, err = Encode(int8(0))
+	require.Nil(t, err)
+
+	cmpResult = bytes.Compare(negative, positive)
+	require.Equal(t, -1, cmpResult)
 }
